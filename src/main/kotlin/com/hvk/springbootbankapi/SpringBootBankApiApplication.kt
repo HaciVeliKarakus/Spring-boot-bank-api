327852a6ef22334308e75b8cbce37658650a3ddb
hacivelikarakus <karakus.haciveli@gmail.com>
2022-08-20 20:01:14 +0300

    init

---
+++ src/main/kotlin/com/hvk/springbootbankapi/SpringBootBankApiApplication.kt
@@ -0,0 +1,11 @@
+package com.hvk.springbootbankapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootBankApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootBankApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/hvk/springbootbankapi/SpringBootBankApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hvk.springbootbankapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootBankApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
